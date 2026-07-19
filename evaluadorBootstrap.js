6aad705d0e43703f7a10f14dedbded6bc7ac4cc9
Garbelt <g.mcmlxxii@gmail.com>
2026-07-19 00:23:47 -0300

    Delete evaluadorBootstrap.js

--- evaluadorBootstrap.js
+++
@@ -1,7 +0,0 @@
-function iniciarEvaluacionCanal(item, fuente) {
-    if (typeof window.iniciarEvaluacionCanalReal === "function") {
-        return window.iniciarEvaluacionCanalReal(item, fuente);
-    }
-
-    console.warn("Evaluador aún no disponible");
-}
