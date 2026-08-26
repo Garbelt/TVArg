ae220cc291013e8ac70b3571b3c388b37280e79c
Garbelt <g.mcmlxxii@gmail.com>
2026-08-26 11:50:32 -0300

    Add files via upload

---
+++ libreria/TVALCs02.js
@@ -0,0 +1,93 @@
+window.TVALC_SERIES.push({
+
+    titulo: "CIA",
+    año: "2025",
+    categoria: "SERIES DE ACCIÓN",
+    tipo: "serie",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/7QuzsX5OE1bNOscdUO6RhzNUJFC.jpg",
+
+    temporadas: [
+
+        {
+            numero: 1,
+
+            capitulos: [
+
+                {
+                    numero: 1,
+                    titulo: "Capítulo 1",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160419.mkv"
+                },
+
+                {
+                    numero: 2,
+                    titulo: "Capítulo 2",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160420.mkv"
+                },
+
+                {
+                    numero: 3,
+                    titulo: "Capítulo 3",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160421.mkv"
+                },
+
+                {
+                    numero: 4,
+                    titulo: "Capítulo 4",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160422.mkv"
+                },
+
+                {
+                    numero: 5,
+                    titulo: "Capítulo 5",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160423.mkv"
+                },
+
+                {
+                    numero: 6,
+                    titulo: "Capítulo 6",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160424.mkv"
+                },
+
+                {
+                    numero: 7,
+                    titulo: "Capítulo 7",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160425.mkv"
+                },
+
+                {
+                    numero: 8,
+                    titulo: "Capítulo 8",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160426.mkv"
+                },
+
+                {
+                    numero: 9,
+                    titulo: "Capítulo 9",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160427.mkv"
+                },
+
+                {
+                    numero: 10,
+                    titulo: "Capítulo 10",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160428.mkv"
+                },
+
+                {
+                    numero: 11,
+                    titulo: "Capítulo 11",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160429.mkv"
+                },
+
+                {
+                    numero: 12,
+                    titulo: "Capítulo 12",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160430.mkv"
+                }
+
+            ]
+        }
+
+    ]
+
+});
