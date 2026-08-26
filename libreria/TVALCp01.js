7f218e80f959bc41a9f0e024a6ad6daa03e78129
Garbelt <g.mcmlxxii@gmail.com>
2026-08-26 01:21:21 -0300

    Add files via upload

---
+++ libreria/TVALCp01.js
@@ -0,0 +1,174 @@
+window.TVALC_PELICULAS = [];
+
+window.TVALC_PELICULAS.push(
+
+{
+    titulo: "Mi mejor amigo, su novia y yo",
+    año: "2026",
+    categoria: "Comedia",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/slZ1c32UqVgl94XV3CMDFjiZdTZ.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160415.mkv"
+},
+{
+    titulo: "Spider-Man: Brand New Day",
+    año: "2026",
+    categoria: "Acción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/9g0sEFhmvmK4nGhXj8DHuv2noYI.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160416.mkv"
+},
+{
+    titulo: "El final de Oak Street",
+    año: "2026",
+    categoria: "Ciencia ficción",
+    poster: "https://es.web.img3.acsta.net/img/61/97/61972d89c1722f4cfaa0301723e7db88.png",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160417.mkv"
+},
+{
+    titulo: "Un talento único",
+    año: "2026",
+    categoria: "Crimen",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/xIRryl8bWHFaMDssKFcPAXhfrRB.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160418.mkv"
+},
+{
+    titulo: "Beast (La bestia)",
+    año: "2026",
+    categoria: "Acción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/3S32pzyZMoOJ3ADrnT2GbV4tiX4.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160402.mkv"
+},
+{
+    titulo: "No Entrar",
+    año: "2026",
+    categoria: "Terror",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/1gHygrBcGU2dL9DHcvnj4FJc8pF.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160403.mkv"
+},
+{
+    titulo: "Nando entre dos mundos",
+    año: "2026",
+    categoria: "Crimen",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/jgIHdtYjvqNRMRcJFVnGUcLmNIb.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160404.mkv"
+},
+{
+    titulo: "The Gates",
+    año: "2026",
+    categoria: "Suspenso",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/dWMiHlKz0OsbVXEEZR3hYyWHcdq.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160401.mkv"
+},
+{
+    titulo: "La invitación",
+    año: "2026",
+    categoria: "Terror",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/21JnfyCARiRkms9AZHtTXiZKbIj.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160398.mkv"
+},
+{
+    titulo: "Jackass: Lo mejor para el final",
+    año: "2026",
+    categoria: "Comedia",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/8sVomxZ7OBnjaHA0C8lAvSq1fr.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160399.mkv"
+},
+{
+    titulo: "Young Washington",
+    año: "2026",
+    categoria: "Historia",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/6CdoTKnRQHJkjRGxTefFGkPQplB.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160400.mkv"
+},
+{
+    titulo: "Las Espadas De Los Guardianes: El viento se levanta en el desierto",
+    año: "2026",
+    categoria: "Acción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/wOYPRKEdYEdTCaJ25Q9IFmsvAkD.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160397.mkv"
+},
+{
+    titulo: "23 000 vidas",
+    año: "2026",
+    categoria: "Drama",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/tZWhuyPxWtODB6rKuGrUUArJq9E.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160396.mkv"
+},
+{
+    titulo: "Un buen chico",
+    año: "2026",
+    categoria: "Drama",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/lrwd5yxWuBjSpnCrzZM1N7obe3K.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160393.mkv"
+},
+{
+    titulo: "La última casa",
+    año: "2026",
+    categoria: "Terror",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/pJnkPtQE8A5H67biPTaTedtHs1L.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160391.mkv"
+},
+{
+    titulo: "La muerte de Robin Hood",
+    año: "2026",
+    categoria: "Aventura",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/pC2hVl4J522GcMVc5OghRHSs0tq.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160392.mkv"
+},
+{
+    titulo: "Leviticus",
+    año: "2026",
+    categoria: "Terror",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/gS8Z2lepduus8x6Crwzb85F6Gli.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/160365.mkv"
+},
+{
+    titulo: "Evil Dead 2",
+    año: "1987",
+    categoria: "Terror",
+    poster: "https://m.media-amazon.com/images/M/MV5BMGMxMWVhMzAtYWRlOS00YzY1LTlkYTQtYjU5ZDM4MGI1YWFjXkEyXkFqcGc@._V1_.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159696.mp4"
+},
+{
+    titulo: "Posesión infernal",
+    año: "1982",
+    categoria: "Terror",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/vgQDogdqPrSk2zRdy2JESq2LGvy.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159697.mp4"
+},
+{
+    titulo: "X-Men: Fénix Oscura",
+    año: "2019",
+    categoria: "Ciencia ficción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/rdByKDkfyVuVSrkllzxKYXiZmTd.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159698.mp4"
+},
+{
+    titulo: "Spider-Man",
+    año: "2002",
+    categoria: "Ciencia ficción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/bCDaKZLRkrVVtPNyHxUfKvepW1N.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159794.mkv"
+},
+{
+    titulo: "Spider-Man 2",
+    año: "2004",
+    categoria: "Ciencia ficción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/tGYrkVuskQKyRZ9WRS6wPTPBJpm.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159795.mkv"
+},
+{
+    titulo: "Spider-Man 3",
+    año: "2007",
+    categoria: "Ciencia ficción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/lkuQzlShE2uzJcsZYU4zBI4VQZP.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159796.mkv"
+},
+{
+    titulo: "Engendro",
+    año: "2026",
+    categoria: "Terror",
+    poster: "https://moviefilms.com.uy/wp-content/uploads/2026/06/Engendro_Arte_640x1138.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159797.mkv"
+},
+
+);
