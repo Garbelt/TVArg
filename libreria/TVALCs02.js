cce8b7fb143b38d899989e718a4c56c8f81324e8
Garbelt <g.mcmlxxii@gmail.com>
2026-08-26 23:00:48 -0300

    Add files via upload

---
+++ libreria/TVALCs02.js
@@ -0,0 +1,359 @@
+window.TVALC_SERIES.push({
+
+    titulo: "CIA",
+    año: "2025",
+    categoria: "SERIES DE ACCIÓN",
+    tipo: "serie",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/7QuzsX5OE1bNOscdUO6RhzNUJFC.jpg",
+
+    temporadas: [
+
+        {
+            numero: 1,
+
+            capitulos: [
+
+                {
+                    numero: 1,
+                    titulo: "Capítulo 1",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160419.mkv"
+                },
+
+                {
+                    numero: 2,
+                    titulo: "Capítulo 2",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160420.mkv"
+                },
+
+                {
+                    numero: 3,
+                    titulo: "Capítulo 3",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160421.mkv"
+                },
+
+                {
+                    numero: 4,
+                    titulo: "Capítulo 4",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160422.mkv"
+                },
+
+                {
+                    numero: 5,
+                    titulo: "Capítulo 5",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160423.mkv"
+                },
+
+                {
+                    numero: 6,
+                    titulo: "Capítulo 6",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160424.mkv"
+                },
+
+                {
+                    numero: 7,
+                    titulo: "Capítulo 7",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160425.mkv"
+                },
+
+                {
+                    numero: 8,
+                    titulo: "Capítulo 8",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160426.mkv"
+                },
+
+                {
+                    numero: 9,
+                    titulo: "Capítulo 9",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160427.mkv"
+                },
+
+                {
+                    numero: 10,
+                    titulo: "Capítulo 10",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160428.mkv"
+                },
+
+                {
+                    numero: 11,
+                    titulo: "Capítulo 11",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160429.mkv"
+                },
+
+                {
+                    numero: 12,
+                    titulo: "Capítulo 12",
+                    url: "http://planettvweb.com:8091/series/Pablo653/FPad2549d/160430.mkv"
+                }
+
+            ]
+        }
+
+    ]
+
+});
+
+
+window.TVALC_SERIES.push({
+
+    titulo: "Shadow Detective",
+    año: "2022",
+    categoria: "Suspenso",
+    tipo: "serie",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/9nErYfGcBn3SixPqSWDS5pk0AOX.jpg",
+
+    temporadas: [
+
+        {
+            numero: 1,
+
+            capitulos: [
+
+                {
+                    numero: 1,
+                    titulo: "Capítulo 1",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1920515.mp4"
+                },
+
+                {
+                    numero: 2,
+                    titulo: "Capítulo 2",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1920575.mp4"
+                },
+
+                {
+                    numero: 3,
+                    titulo: "Capítulo 3",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1920589.mp4"
+                },
+
+                {
+                    numero: 4,
+                    titulo: "Capítulo 4",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1920574.mp4"
+                },
+
+                {
+                    numero: 5,
+                    titulo: "Capítulo 5",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1920578.mp4"
+                },
+
+                {
+                    numero: 6,
+                    titulo: "Capítulo 6",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1920572.mp4"
+                }
+
+            ]
+        }
+
+    ]
+
+});
+
+
+window.TVALC_SERIES.push({
+
+    titulo: "Stan Lee's Lucky Man",
+    año: "2016",
+    categoria: "Crimen",
+    tipo: "serie",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/vkYlRsOjJ82uKIlxwonaTSVx0ng.jpg",
+
+    temporadas: [
+
+        {
+            numero: 1,
+
+            capitulos: [
+
+                {
+                    numero: 1,
+                    titulo: "Capítulo 1",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1921799.mkv"
+                },
+
+                {
+                    numero: 2,
+                    titulo: "Capítulo 2",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1921846.mkv"
+                },
+
+                {
+                    numero: 3,
+                    titulo: "Capítulo 3",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1921849.mkv"
+                },
+
+                {
+                    numero: 4,
+                    titulo: "Capítulo 4",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1921848.mkv"
+                },
+
+                {
+                    numero: 5,
+                    titulo: "Capítulo 5",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1921850.mkv"
+                },
+
+                {
+                    numero: 6,
+                    titulo: "Capítulo 6",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1921851.mkv"
+                },
+
+                {
+                    numero: 7,
+                    titulo: "Capítulo 7",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1921940.mkv"
+                },
+
+                {
+                    numero: 8,
+                    titulo: "Capítulo 8",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1921938.mkv"
+                },
+
+                {
+                    numero: 9,
+                    titulo: "Capítulo 9",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1921942.mkv"
+                },
+
+                {
+                    numero: 10,
+                    titulo: "Capítulo 10",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1921936.mkv"
+                }
+
+            ]
+        },
+
+        {
+            numero: 2,
+
+            capitulos: [
+
+                {
+                    numero: 1,
+                    titulo: "Capítulo 1",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922001.mkv"
+                },
+
+                {
+                    numero: 2,
+                    titulo: "Capítulo 2",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922004.mkv"
+                },
+
+                {
+                    numero: 3,
+                    titulo: "Capítulo 3",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922003.mkv"
+                },
+
+                {
+                    numero: 4,
+                    titulo: "Capítulo 4",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922007.mkv"
+                },
+
+                {
+                    numero: 5,
+                    titulo: "Capítulo 5",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922067.mkv"
+                },
+
+                {
+                    numero: 6,
+                    titulo: "Capítulo 6",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922066.mkv"
+                },
+
+                {
+                    numero: 7,
+                    titulo: "Capítulo 7",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922071.mkv"
+                },
+
+                {
+                    numero: 8,
+                    titulo: "Capítulo 8",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922075.mkv"
+                },
+
+                {
+                    numero: 9,
+                    titulo: "Capítulo 9",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922135.mkv"
+                },
+
+                {
+                    numero: 10,
+                    titulo: "Capítulo 10",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922134.mkv"
+                }
+
+            ]
+        },
+
+        {
+            numero: 3,
+
+            capitulos: [
+
+                {
+                    numero: 1,
+                    titulo: "Capítulo 1",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922151.mkv"
+                },
+
+                {
+                    numero: 2,
+                    titulo: "Capítulo 2",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922152.mkv"
+                },
+
+                {
+                    numero: 3,
+                    titulo: "Capítulo 3",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922206.mkv"
+                },
+
+                {
+                    numero: 4,
+                    titulo: "Capítulo 4",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922211.mkv"
+                },
+
+                {
+                    numero: 5,
+                    titulo: "Capítulo 5",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922223.mkv"
+                },
+
+                {
+                    numero: 6,
+                    titulo: "Capítulo 6",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922221.mkv"
+                },
+
+                {
+                    numero: 7,
+                    titulo: "Capítulo 7",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922262.mkv"
+                },
+
+                {
+                    numero: 8,
+                    titulo: "Capítulo 8",
+                    url: "http://liontv.es:8080/series/Carlos3750/6u3rx2/1922268.mkv"
+                }
+
+            ]
+        }
+
+    ]
+
+});
