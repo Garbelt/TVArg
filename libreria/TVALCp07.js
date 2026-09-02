f7ea8243f869efa6285ea1c80b38677c004b8eeb
Garbelt <g.mcmlxxii@gmail.com>
2026-09-02 12:19:26 -0300

    Add files via upload

---
+++ libreria/TVALCp07.js
@@ -0,0 +1,307 @@
+window.TVALC_PELICULAS.push(
+
+{
+    titulo: "Posesión infernal: En llamas",
+    año: "2026",
+    categoria: "Terror",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/hDXmoQJ7EClgjv83Z0vEIQu2t7S.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159695.mkv"
+},
+
+{
+    titulo: "Soulm8te",
+    año: "2026",
+    categoria: "Ciencia ficción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/bNErActDctl6cdUGw9pnjSCmyhQ.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159671.mkv"
+},
+
+{
+    titulo: "Spider-Man: Un nuevo día",
+    año: "2026",
+    categoria: "Ciencia ficción",
+    poster: "https://m.media-amazon.com/images/M/MV5BOWNjYWM3NWItOGE0ZS00MWRjLThiZWEtYjc4ZmNmMmU5ZTVmXkEyXkFqcGc@._V1_.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159670.mkv"
+},
+
+{
+    titulo: "El cobrador de deudas",
+    año: "2026",
+    categoria: "Acción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/loW8i6T3amH4CONDjaUNmQv9kva.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159569.mkv"
+},
+
+{
+    titulo: "La Boca del Diablo",
+    año: "2026",
+    categoria: "Terror",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/cPsXFr7g39Kjul7Wx8XBpZwkOS4.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159561.mkv"
+},
+
+{
+    titulo: "Spider-Man: Lejos de casa",
+    año: "2019",
+    categoria: "Ciencia ficción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/o59zDVJDIehGcWZsMUGU5SN0V0D.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159558.mkv"
+},
+
+{
+    titulo: "Spider-Man: De regreso a casa",
+    año: "2017",
+    categoria: "Ciencia ficción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/bRl6C6FwzTndk0MBGZZ68nRFlw3.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159559.mkv"
+},
+
+{
+    titulo: "Spider-Man: Sin camino a casa",
+    año: "2021",
+    categoria: "Ciencia ficción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/accAPImdjUwgNgocDNWXCGFEcvL.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159560.mkv"
+},
+
+{
+    titulo: "Supergirl",
+    año: "2026",
+    categoria: "Acción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/sbPxtcrj3fPp3CH4CZgSj9Or7Yr.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159535.mkv"
+},
+
+{
+    titulo: "La Odisea",
+    año: "2026",
+    categoria: "Aventura",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/sf2ke1nwFOLLcwLZiXu9VaMMv8e.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159432.mkv"
+},
+
+{
+    titulo: "Elize: Sombras de una mujer",
+    año: "2026",
+    categoria: "Suspenso",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/a3y0aWIJdJEw7pme6Y9W0aReIN9.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159451.mkv"
+},
+
+{
+    titulo: "Ballistic",
+    año: "2026",
+    categoria: "Acción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/f8GUHZjNPSGLjUhi7HzVn9541bY.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159276.mkv"
+},
+
+{
+    titulo: "Temporada de sangre",
+    año: "2026",
+    categoria: "Terror",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/lZZu0UEPtEeZ90fkX6i1pmEHOs8.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159277.mkv"
+},
+
+{
+    titulo: "Dolly",
+    año: "2026",
+    categoria: "Suspenso",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/umDXoAiEMb7RxB4gaPAlT02XLAS.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159257.mkv"
+},
+
+{
+    titulo: "Hermanito",
+    año: "2026",
+    categoria: "Comedia",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/s8mudAYQcHmVoFSamqyv1Ron527.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159157.mkv"
+},
+
+{
+    titulo: "En prácticas",
+    año: "2026",
+    categoria: "Comedia",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/fX5HV8LC2tulHI2GT9Ar8WVFyg.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159158.mkv"
+},
+
+{
+    titulo: "Obsesión",
+    año: "2026",
+    categoria: "Suspenso",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/ohi9xvbBUymM4SuIOSlt1xbLRQQ.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159069.mkv"
+},
+
+{
+    titulo: "Off the Grid",
+    año: "2025",
+    categoria: "Acción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/mMUVnkcx2VawTBJYnnnRN8fxJpb.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159064.mkv"
+},
+
+{
+    titulo: "Rescate en el Golfo",
+    año: "2025",
+    categoria: "Drama",
+    poster: "https://m.media-amazon.com/images/S/pv-target-images/1dd4bc4116d0b59ecf505dc382987a640bf894ce5e53e44b22cf0c3daf920b13.png",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159452.mkv"
+},
+
+{
+    titulo: "72 horas",
+    año: "2026",
+    categoria: "Comedia",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/aaH93fUARqbfvM9XtaKybUAIo9u.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159453.mkv"
+},
+
+{
+    titulo: "Ángeles de guerra",
+    año: "2026",
+    categoria: "Bélica",
+    poster: "https://m.media-amazon.com/images/S/pv-target-images/a526082c54f3e432c0e8f15114f939b989ec9b6caa86fe3598f2bd618d76701d.png",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159454.mkv"
+},
+
+{
+    titulo: "Los creyentes",
+    año: "2026",
+    categoria: "Drama",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/pUvs1iFFMDy3sNp5dNmkRak1oxD.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159455.mkv"
+},
+
+{
+    titulo: "Deadpool & Wolverine",
+    año: "2024",
+    categoria: "Ciencia ficción",
+    poster: "https://mx.web.img2.acsta.net/c_310_420/img/86/2a/862ad21350d9a8a1ee76ca29dddf0aca.jpeg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159341.mkv"
+},
+
+{
+    titulo: "Deseo",
+    año: "2026",
+    categoria: "Suspenso",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/5lJPvf7cJ2r2EiNrnvBVYpusKFM.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159389.mkv"
+},
+
+{
+    titulo: "Nada entre los dos",
+    año: "2026",
+    categoria: "Romance",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/6GZt6CfXJQgOFBA7ArLbOQYvXI8.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159390.mkv"
+},
+
+{
+    titulo: "La voluntad de Dios",
+    año: "2026",
+    categoria: "Drama",
+    poster: "https://m.media-amazon.com/images/S/pv-target-images/81e9607fd7207a4a6f372a204d1e964233c164a75258cfddbfb2e820e9fb018e.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159337.mkv"
+},
+
+{
+    titulo: "Pitfall",
+    año: "2025",
+    categoria: "Suspenso",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/xlkd8rmU3DMFKqCGOchAH1avrfM.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159339.mkv"
+},
+
+{
+    titulo: "The Breadwinner",
+    año: "2026",
+    categoria: "Comedia",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/qox5OvknYs1XJqw3aS3d9CvSEKO.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159340.mkv"
+},
+
+{
+    titulo: "Amarga Navidad",
+    año: "2026",
+    categoria: "Drama",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/ji29dXqoeIsQiVnXAkzI4nzxMjr.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159333.mkv"
+},
+
+{
+    titulo: "Couture: Vidas entrelazadas",
+    año: "2026",
+    categoria: "Drama",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/ivXtlBv1UhZhAdIj2OouEAOuGf1.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159334.mkv"
+},
+
+{
+    titulo: "Maridos en acción",
+    año: "2026",
+    categoria: "Comedia",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/12TfQSbwyb3t9PN0KTOTE4NB7lq.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159036.mkv"
+},
+
+{
+    titulo: "He-Man y los Masters del Universo",
+    año: "2026",
+    categoria: "Fantasía",
+    poster: "https://m.media-amazon.com/images/M/MV5BMTM0ODRiMGItMTA3OC00NGI3LTllZTctNGY3NzllMmRjZTNhXkEyXkFqcGc@._V1_.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159032.mkv"
+},
+
+{
+    titulo: "Parto Maldito",
+    año: "2025",
+    categoria: "Terror",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/4ozeV62H7CGWprsCrif2lax8dHO.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159034.mkv"
+},
+
+{
+    titulo: "Van Helsing",
+    año: "2004",
+    categoria: "Fantasía",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/fqorDWIwBBwMvLTZXxUWgrEyGWy.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159030.mkv"
+},
+
+{
+    titulo: "El cuchitril de Joe",
+    año: "1996",
+    categoria: "Comedia",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/8odl5dqj0xu5dUXIJ25DJGbwn5I.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159025.mkv"
+},
+
+{
+    titulo: "El santuario",
+    año: "2011",
+    categoria: "Aventura",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/mGgQ5Z7FAgrCsBIPQEQQHDA63FK.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159029.mkv"
+},
+
+{
+    titulo: "Culpa tuya: Londres",
+    año: "2026",
+    categoria: "Romance",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/oeOmayFaiGz6km5vShPpDBQnZf1.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159021.mkv"
+},
+
+{
+    titulo: "Buena suerte, pásalo bien, no mueras",
+    año: "2025",
+    categoria: "Ciencia ficción",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/vzJkrTx01ol7FTIhzS50pms5xEG.jpg",
+    url: "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159020.mkv"
+}
+
+);
