696e8017946abae631f019bd622e46a1e2c4b37c
Garbelt <g.mcmlxxii@gmail.com>
2026-09-01 22:08:04 -0300

    Add files via upload

---
+++ libreria/TVALCs03.js
@@ -0,0 +1,466 @@
+window.TVALC_SERIES.push({
+
+titulo: "Bloque de Búsqueda",
+año: "2016",
+categoria: "Crimen",
+tipo: "serie",
+poster: "http://redworld.pro:8880/images/82012e668dc7ce187094e88f2ba5a8a0.jpg",
+
+temporadas: [
+
+    {
+        numero: 1,
+
+        capitulos: [
+
+            {
+                numero: 1,
+                titulo: "Capítulo 1",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471961.mp4"
+            },
+
+            {
+                numero: 2,
+                titulo: "Capítulo 2",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471962.mp4"
+            },
+
+            {
+                numero: 3,
+                titulo: "Capítulo 3",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471963.mp4"
+            },
+
+            {
+                numero: 4,
+                titulo: "Capítulo 4",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471964.mp4"
+            },
+
+            {
+                numero: 5,
+                titulo: "Capítulo 5",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471965.mp4"
+            },
+
+            {
+                numero: 6,
+                titulo: "Capítulo 6",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471966.mp4"
+            },
+
+            {
+                numero: 7,
+                titulo: "Capítulo 7",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471967.mp4"
+            },
+
+            {
+                numero: 8,
+                titulo: "Capítulo 8",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471968.mp4"
+            },
+
+            {
+                numero: 9,
+                titulo: "Capítulo 9",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471969.mp4"
+            },
+
+            {
+                numero: 10,
+                titulo: "Capítulo 10",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471970.mp4"
+            },
+
+            {
+                numero: 11,
+                titulo: "Capítulo 11",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471971.mp4"
+            },
+
+            {
+                numero: 12,
+                titulo: "Capítulo 12",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471972.mp4"
+            },
+
+            {
+                numero: 13,
+                titulo: "Capítulo 13",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471973.mp4"
+            },
+
+            {
+                numero: 14,
+                titulo: "Capítulo 14",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471974.mp4"
+            },
+
+            {
+                numero: 15,
+                titulo: "Capítulo 15",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471975.mp4"
+            },
+
+            {
+                numero: 16,
+                titulo: "Capítulo 16",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471976.mp4"
+            },
+
+            {
+                numero: 17,
+                titulo: "Capítulo 17",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471977.mp4"
+            },
+
+            {
+                numero: 18,
+                titulo: "Capítulo 18",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471978.mp4"
+            },
+
+            {
+                numero: 19,
+                titulo: "Capítulo 19",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471979.mp4"
+            },
+
+            {
+                numero: 20,
+                titulo: "Capítulo 20",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471980.mp4"
+            },
+
+            {
+                numero: 21,
+                titulo: "Capítulo 21",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471981.mp4"
+            },
+
+            {
+                numero: 22,
+                titulo: "Capítulo 22",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471982.mp4"
+            },
+
+            {
+                numero: 23,
+                titulo: "Capítulo 23",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471983.mp4"
+            },
+
+            {
+                numero: 24,
+                titulo: "Capítulo 24",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471984.mp4"
+            },
+
+            {
+                numero: 25,
+                titulo: "Capítulo 25",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471985.mp4"
+            },
+
+            {
+                numero: 26,
+                titulo: "Capítulo 26",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471986.mp4"
+            },
+
+            {
+                numero: 27,
+                titulo: "Capítulo 27",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471987.mp4"
+            },
+
+            {
+                numero: 28,
+                titulo: "Capítulo 28",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471988.mp4"
+            },
+
+            {
+                numero: 29,
+                titulo: "Capítulo 29",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471989.mp4"
+            },
+
+            {
+                numero: 30,
+                titulo: "Capítulo 30",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471990.mp4"
+            },
+
+            {
+                numero: 31,
+                titulo: "Capítulo 31",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471991.mp4"
+            },
+
+            {
+                numero: 32,
+                titulo: "Capítulo 32",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471992.mp4"
+            },
+
+            {
+                numero: 33,
+                titulo: "Capítulo 33",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471993.mp4"
+            },
+
+            {
+                numero: 34,
+                titulo: "Capítulo 34",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471994.mp4"
+            },
+
+            {
+                numero: 35,
+                titulo: "Capítulo 35",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471995.mp4"
+            },
+
+            {
+                numero: 36,
+                titulo: "Capítulo 36",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471996.mp4"
+            },
+
+            {
+                numero: 37,
+                titulo: "Capítulo 37",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471997.mp4"
+            },
+
+            {
+                numero: 38,
+                titulo: "Capítulo 38",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471998.mp4"
+            },
+
+            {
+                numero: 39,
+                titulo: "Capítulo 39",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/471999.mp4"
+            },
+
+            {
+                numero: 40,
+                titulo: "Capítulo 40",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472000.mp4"
+            },
+
+            {
+                numero: 41,
+                titulo: "Capítulo 41",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472001.mp4"
+            },
+
+            {
+                numero: 42,
+                titulo: "Capítulo 42",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472002.mp4"
+            },
+
+            {
+                numero: 43,
+                titulo: "Capítulo 43",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472003.mp4"
+            },
+
+            {
+                numero: 44,
+                titulo: "Capítulo 44",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472004.mp4"
+            },
+
+            {
+                numero: 45,
+                titulo: "Capítulo 45",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472005.mp4"
+            },
+
+            {
+                numero: 46,
+                titulo: "Capítulo 46",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472006.mp4"
+            },
+
+            {
+                numero: 47,
+                titulo: "Capítulo 47",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472007.mp4"
+            },
+
+            {
+                numero: 48,
+                titulo: "Capítulo 48",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472008.mp4"
+            },
+
+            {
+                numero: 49,
+                titulo: "Capítulo 49",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472009.mp4"
+            },
+
+            {
+                numero: 50,
+                titulo: "Capítulo 50",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472010.mp4"
+            },
+
+            {
+                numero: 51,
+                titulo: "Capítulo 51",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472011.mp4"
+            },
+
+            {
+                numero: 52,
+                titulo: "Capítulo 52",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472012.mp4"
+            },
+
+            {
+                numero: 53,
+                titulo: "Capítulo 53",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472013.mp4"
+            },
+
+            {
+                numero: 54,
+                titulo: "Capítulo 54",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472014.mp4"
+            },
+
+            {
+                numero: 55,
+                titulo: "Capítulo 55",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472015.mp4"
+            },
+
+            {
+                numero: 56,
+                titulo: "Capítulo 56",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472016.mp4"
+            },
+
+            {
+                numero: 57,
+                titulo: "Capítulo 57",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472017.mp4"
+            },
+
+            {
+                numero: 58,
+                titulo: "Capítulo 58",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472018.mp4"
+            },
+
+            {
+                numero: 59,
+                titulo: "Capítulo 59",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472019.mp4"
+            },
+
+            {
+                numero: 60,
+                titulo: "Capítulo 60",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472020.mp4"
+            },
+
+            {
+                numero: 61,
+                titulo: "Capítulo 61",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472021.mp4"
+            },
+
+            {
+                numero: 62,
+                titulo: "Capítulo 62",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472022.mp4"
+            },
+
+            {
+                numero: 63,
+                titulo: "Capítulo 63",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472023.mp4"
+            },
+
+            {
+                numero: 64,
+                titulo: "Capítulo 64",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472024.mp4"
+            },
+
+            {
+                numero: 65,
+                titulo: "Capítulo 65",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472025.mp4"
+            },
+
+            {
+                numero: 66,
+                titulo: "Capítulo 66",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472026.mp4"
+            },
+
+            {
+                numero: 67,
+                titulo: "Capítulo 67",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472027.mp4"
+            },
+
+            {
+                numero: 68,
+                titulo: "Capítulo 68",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472028.mp4"
+            },
+
+            {
+                numero: 69,
+                titulo: "Capítulo 69",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472029.mp4"
+            },
+
+            {
+                numero: 70,
+                titulo: "Capítulo 70",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472030.mp4"
+            },
+
+            {
+                numero: 71,
+                titulo: "Capítulo 71",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472031.mp4"
+            },
+
+            {
+                numero: 72,
+                titulo: "Capítulo 72",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472032.mp4"
+            },
+
+            {
+                numero: 73,
+                titulo: "Capítulo 73",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472033.mp4"
+            },
+
+            {
+                numero: 74,
+                titulo: "Capítulo 74",
+                url: "http://redworld.pro:8880/series/@Gansowj9999/4uPsB5ESWBUG/472034.mp4"
+            }
+
+        ]
+
+    }
+
+]
+
+});
