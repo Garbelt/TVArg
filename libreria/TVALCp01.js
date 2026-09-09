d2a1466ab82e27ed854ea5cd8059fd14ecceff67
Garbelt <g.mcmlxxii@gmail.com>
2026-09-08 21:13:19 -0300

    Add files via upload

---
+++ libreria/TVALCp01.js
@@ -0,0 +1,134 @@
+window.TVALC_PELICULAS = [];
+
+TVALC_PELICULAS.push(
+{
+    "titulo": "La momia de Lee Cronin",
+    "año": "2026",
+    "categoria": "Terror",
+    "clasificacion": "MAYORES DE 18",
+    "tipo": "pelicula",
+    "poster": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/sKYkjA9H4Qlzz5M1oqGCFVsTOfL.jpg",
+    "descripcion": "Una antigua momia despierta y desata una aterradora amenaza sobrenatural, obligando a quienes se encuentran en su camino a enfrentarse a una fuerza maligna vinculada a un oscuro pasado.",
+    "url": "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159015.mkv"
+},
+{
+    "titulo": "911: Llamada infernal",
+    "año": "2026",
+    "categoria": "Terror",
+    "clasificacion": "MAYORES DE 16",
+    "tipo": "pelicula",
+    "poster": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/aOIXYOwB97ioUnhHzwnyCRxwVBe.jpg",
+    "descripcion": "Una llamada de emergencia lleva a unos policías hasta una situación aparentemente rutinaria que termina convirtiéndose en una aterradora pesadilla. Mientras intentan descubrir qué está ocurriendo, quedan atrapados en una amenaza relacionada con un peligroso culto.",
+    "url": "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159016.mkv"
+},
+{
+    "titulo": "Cuenta atrás",
+    "año": "2026",
+    "categoria": "Acción",
+    "clasificacion": "MAYORES DE 16",
+    "tipo": "pelicula",
+    "poster": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/m3On2pUX6KRJwI8Fp7xqJkKQdIv.jpg",
+    "descripcion": "Un agente de policía se ve involucrado en una peligrosa carrera contra el tiempo cuando una amenaza pone en riesgo la seguridad de la ciudad. Para evitar una tragedia deberá seguir una serie de pistas y enfrentarse a enemigos cada vez más peligrosos.",
+    "url": "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159018.mkv"
+},
+{
+    "titulo": "El engaño",
+    "año": "2026",
+    "categoria": "Suspenso",
+    "clasificacion": "MAYORES DE 16",
+    "tipo": "pelicula",
+    "poster": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/y8ADH1i7Ti1A0WLWjMCTEN6s4F8.jpg",
+    "descripcion": "Una persona descubre que nada es lo que parece cuando una serie de acontecimientos inesperados comienza a revelar secretos cuidadosamente ocultos. A medida que la verdad sale a la luz, deberá descubrir en quién puede confiar.",
+    "url": "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159019.mkv"
+},
+{
+    "titulo": "Psycho Killer",
+    "año": "2026",
+    "categoria": "Crimen",
+    "clasificacion": "MAYORES DE 18",
+    "tipo": "pelicula",
+    "poster": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/5xgxxmLivJXL8aF0HdZfpx8aAIo.jpg",
+    "descripcion": "Un asesino despiadado comienza una serie de crímenes que mantiene aterrorizada a la población. Mientras la policía intenta detenerlo, una investigación cada vez más peligrosa revela una conexión inesperada detrás de los asesinatos.",
+    "url": "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159008.mkv"
+},
+{
+    "titulo": "The Internship",
+    "año": "2026",
+    "categoria": "Comedia",
+    "clasificacion": "MAYORES DE 13",
+    "tipo": "pelicula",
+    "poster": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/fYqSOkix4rbDiZW0ACNnvZCpT6X.jpg",
+    "descripcion": "Un grupo de jóvenes se enfrenta a las dificultades y situaciones inesperadas de una experiencia laboral que promete ser una oportunidad para comenzar sus carreras. Entre desafíos, amistades y conflictos, deberán aprender a desenvolverse en el competitivo mundo profesional.",
+    "url": "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159006.mkv"
+},
+{
+    "titulo": "Backrooms",
+    "año": "2026",
+    "categoria": "Terror",
+    "clasificacion": "MAYORES DE 13",
+    "tipo": "pelicula",
+    "poster": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/u3CfgxtnZRxbIm8JuVTzt3DXQhg.jpg",
+    "descripcion": "Un joven queda atrapado en un misterioso laberinto de espacios interminables conocido como las Backrooms. Mientras busca una salida, deberá enfrentarse a lugares cada vez más inquietantes y a una presencia aterradora que parece acecharlo.",
+    "url": "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159003.mkv"
+},
+{
+    "titulo": "Scary Movie",
+    "año": "2026",
+    "categoria": "Comedia",
+    "clasificacion": "MAYORES DE 18",
+    "tipo": "pelicula",
+    "poster": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/qhGf7btEU9hcGz3A0lneNptJKL7.jpg",
+    "descripcion": "Una nueva generación de personajes se ve envuelta en una disparatada historia que parodia las películas de terror más conocidas, combinando situaciones absurdas, humor irreverente y referencias a clásicos del género.",
+    "url": "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159004.mkv"
+},
+{
+    "titulo": "El secreto del abismo",
+    "año": "1989",
+    "categoria": "Ciencia ficción",
+    "clasificacion": "MAYORES DE 13",
+    "tipo": "pelicula",
+    "poster": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/3IF8MSSy6lo2pg7dfa1n9n8SeUX.jpg",
+    "descripcion": "Un equipo de trabajadores de una plataforma submarina es enviado a investigar un submarino nuclear hundido. En las profundidades del océano descubren una misteriosa forma de vida que cambiará por completo su misión.",
+    "url": "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159000.mkv"
+},
+{
+    "titulo": "Exit 8",
+    "año": "2025",
+    "categoria": "Terror",
+    "clasificacion": "MAYORES DE 13",
+    "tipo": "pelicula",
+    "poster": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/mqlgMvy1d5jIbIE6SPlJq724htT.jpg",
+    "descripcion": "Un hombre queda atrapado en un interminable pasillo de una estación de metro. Para encontrar la salida deberá observar cuidadosamente su entorno y detectar cualquier anomalía, ya que cometer un error puede devolverlo al comienzo del recorrido.",
+    "url": "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/159001.mkv"
+},
+{
+    "titulo": "La guerra del mañana",
+    "año": "2021",
+    "categoria": "Ciencia ficción",
+    "clasificacion": "MAYORES DE 16",
+    "tipo": "pelicula",
+    "poster": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/tzp6VzED2TBc02bkYoYnQa6r2OK.jpg",
+    "descripcion": "En el futuro, la humanidad está perdiendo una guerra contra una especie alienígena. Un grupo de personas del presente es enviado al futuro para combatir junto a los soldados que intentan evitar la extinción de la humanidad.",
+    "url": "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/158997.mkv"
+},
+{
+    "titulo": "La llegada",
+    "año": "2016",
+    "categoria": "Ciencia ficción",
+    "clasificacion": "MAYORES DE 13",
+    "tipo": "pelicula",
+    "poster": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/tbVITeytclB4JKx2LxxasrCCmZx.jpg",
+    "descripcion": "Cuando misteriosas naves extraterrestres aparecen en distintos lugares del mundo, una lingüista es convocada para intentar comunicarse con sus ocupantes. Mientras las naciones buscan comprender las intenciones de los visitantes, ella descubre que el lenguaje puede cambiar su percepción del tiempo y de la realidad.",
+    "url": "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/158998.mkv"
+},
+{
+    "titulo": "Señales",
+    "año": "2002",
+    "categoria": "Ciencia ficción",
+    "clasificacion": "MAYORES DE 13",
+    "tipo": "pelicula",
+    "poster": "https://m.media-amazon.com/images/M/MV5BNTJiZGZkODAtYWVkMi00MGM4LTkzODEtZDU3YjEzYTRmYjAxXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    "descripcion": "Una familia que vive en una granja comienza a descubrir extrañas señales y enormes figuras dibujadas en sus campos. Mientras intentan comprender el origen de estos fenómenos, acontecimientos cada vez más inquietantes hacen pensar que algo desconocido se aproxima.",
+    "url": "http://planettvweb.com:8091/movie/Pablo653/FPad2549d/158999.mkv"
+}
+);
