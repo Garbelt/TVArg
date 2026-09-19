dec10835cf1ca2c0a2d87e9fc68bc99496a1686a
Garbelt <g.mcmlxxii@gmail.com>
2026-09-18 22:36:25 -0300

    Add files via upload

---
+++ libreria/TVALCp10.js
@@ -0,0 +1,11 @@
+TVALC_PELICULAS.push(
+{
+    "titulo": "Queen – Live in Budapest 1986",
+    "año": "2012",
+    "poster": "https://image.tmdb.org/t/p/w500/dCsCez4aVeXqMiALdWYp4LFsztI.jpg",
+    "url": "https://dn720305.ca.archive.org/0/items/queen-live-in-budapest-1986/QUEEN%20-%20Live%20In%20Budapest%201986.mp4",
+    "categoria": "Musical, Documental",
+    "clasificacion": "ATP",
+    "descripcion": "El 27 de julio de 1986, la banda británica de rock Queen abrió nuevos caminos tocando por primera vez en Hungría, un país que todavía estaba bajo una dictadura comunista detrás del Telón de Acero. El concierto, enmarcado en el \"Magic Tour\", se celebró en el Nepstadion (hoy Estadio Ferenc Puskás) en la ciudad de Budapest ante un público de 80.000 personas. La película muestra imágenes de los eventos previos al concierto y momentos vividos por cada uno de los miembros de la banda en la ciudad de Budapest."
+}
+);
