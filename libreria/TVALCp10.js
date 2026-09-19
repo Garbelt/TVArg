78ae2dbd693a17518e57b8f4d743c90a24c65878
Garbelt <g.mcmlxxii@gmail.com>
2026-09-18 22:51:56 -0300

    Update movie entries in TVALC_PELICULAS

--- libreria/TVALCp10.js
+++ libreria/TVALCp10.js
@@ -1,11 +1,21 @@
 TVALC_PELICULAS.push(
 {
-    "titulo": "Queen – Live in Budapest 1986",
+    "titulo": "Queen – Live in Budapest",
     "año": "2012",
     "poster": "https://image.tmdb.org/t/p/w500/dCsCez4aVeXqMiALdWYp4LFsztI.jpg",
     "url": "https://dn720305.ca.archive.org/0/items/queen-live-in-budapest-1986/QUEEN%20-%20Live%20In%20Budapest%201986.mp4",
-    "categoria": "Musical, Documental",
+    "categoria": "Recital, Musical, Documental",
     "clasificacion": "ATP",
     "descripcion": "El 27 de julio de 1986, la banda británica de rock Queen abrió nuevos caminos tocando por primera vez en Hungría, un país que todavía estaba bajo una dictadura comunista detrás del Telón de Acero. El concierto, enmarcado en el \"Magic Tour\", se celebró en el Nepstadion (hoy Estadio Ferenc Puskás) en la ciudad de Budapest ante un público de 80.000 personas. La película muestra imágenes de los eventos previos al concierto y momentos vividos por cada uno de los miembros de la banda en la ciudad de Budapest."
+},
+{
+    "titulo": "Pink Floyd – Delicate Sound of Thunder",
+    "año": "1989",
+    "poster": "https://image.tmdb.org/t/p/w500/8AR6gmMS3h0uedYr0BAI1yHGaHh.jpg",
+    "url": "https://dn710005.ca.archive.org/0/items/ARTE_Concert_Pink-Floyd_Delicate-Sound-of-Thunder_1988_2019/ARTE-Concert_Pink-Floyd_Delicate-Sound-of-Thunder_1988_2019.mp4",
+    "categoria": "Recital, Musical, Documental",
+    "clasificacion": "ATP",
+    "descripcion": "Delicate Sound of Thunder es un video del concierto de Pink Floyd tomado de la gira A Momentary Lapse of Reason. Fue filmado en gran parte durante los conciertos del 19 de agosto de 1988 al 23 de agosto de 1988 en el Nassau Coliseum en Uniondale, Nueva York, con algunas imágenes adicionales del 21 de junio de 1988 y el 22 de junio de 1988 en la Place d'Armes de la Château de Versailles, Versailles, Francia (utilizado para proporcionar la actuación de \"El gran concierto en el cielo\"). (crédito - wiki)"
 }
+
 );
