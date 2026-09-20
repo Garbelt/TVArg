80de933fc3dc8d564d975f578ef400c8b77d58b9
Garbelt <g.mcmlxxii@gmail.com>
2026-09-19 22:30:55 -0300

    Update TVALCp10.js

--- libreria/TVALCp10.js
+++ libreria/TVALCp10.js
@@ -25,6 +25,15 @@ TVALC_PELICULAS.push(
     "categoria": "Recital, Musical",
     "clasificacion": "ATP",
     "descripcion": "Registro audiovisual de la 18.ª Internationale Jazzwoche Burghausen, celebrada en Alemania en 1987. Una selección de actuaciones que reúne a destacados exponentes del jazz, blues y swing, entre ellos James Cotton Blues Band, Dudu Pukwana y Zila, Bob Kerr's Whoopee Band y Ferdinand Havlik Swing Big Band. Material histórico producido por Bayerischer Rundfunk (BR-alpha)."
+},
+{
+    "titulo": "Lawrence - Festival de Jazz de Leverkusen",
+    "año": "2025",
+    "poster": "https://image.tmdb.org/t/p/w500/1secgSGRsQpT4iCZgoYcus0enGF.jpg",
+    "url": "https://arte-cmafhls.akamaized.net/am/cmaf-concert-webonly/128000/128800/128862-012-A_VO/20251209178632BC8833FA84E4ED295F419C0FD103/medias/128862-012-A_v1080.mp4",
+    "audioUrl": "https://arte-cmafhls.akamaized.net/am/cmaf-concert-webonly/128000/128800/128862-012-A_VO/20251209178632BC8833FA84E4ED295F419C0FD103/medias/128862-012-A_aud_VO-MUS_1.mp4",
+    "categoria": "Musical",
+    "clasificacion": "ATP",
+    "descripcion": "Este grupo de soul-pop estadounidense, liderado por el dúo que forman los hermanos Clyde y Gracie Lawrence, se dio a conocer en los programas de TV de Jimmy Fallon y Kelly Clarkson. Han compartido escenario con los Rolling Stones, los Jonas Brothers o Vulfpeck, y se han presentado en festivales importantes como Coachella o Bonnaroo."
 }
-
 );
